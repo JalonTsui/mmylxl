8f6fe07150698a856a84746dbc2e7d6de5ad39e2
jalontsui <443415003@qq.com>
2025-05-27 00:57:07 +0800

    MyArrayList interface

---
+++ datastructures/src/main/java/org/jalontsui/datastructures/list/MyArrayList.java
@@ -0,0 +1,64 @@
+package org.jalontsui.datastructures.list;
+
+/*
+ * @author JalonTsui
+ * @Date 0:36 2025/5/27
+ **/
+
+public interface MyArrayList<T> extends Iterable<T> {
+
+    /**
+     * 清空list
+     */
+    void clear();
+
+    /**
+     * 获取list当前元素个数
+     * @return 当前元素个数
+     */
+    int size();
+
+    /**
+     * 判断list是否为空
+     * @return list是否为空
+     */
+    boolean isEmpty();
+
+    /**
+     * 获取list索引对应值
+     * @param index 索引
+     * @return 索引对应值
+     */
+    T get(int index);
+
+    /**
+     * 给list指定索引赋值
+     * @param index 索引
+     * @param newVal 插入的元素
+     * @return 若该索引已经有值则返回久的值
+     */
+    T set(int index, T newVal);
+
+    /**
+     * list末端添加元素
+     * @param item 添加元素
+     * @return 添加结果
+     */
+    boolean add(T item);
+
+    /**
+     * 指定位置插入元素
+     * @param index 索引
+     * @param item 插入的值
+     */
+    void add(int index, T item);
+
+    /**
+     * 移除指定索引值
+     * @param index
+     * @return
+     */
+    T remove(int index);
+
+
+}
