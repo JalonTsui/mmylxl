dd8e2c59a371425074ca4c26d8aa08e54c5cb472
jalontsui <443415003@qq.com>
2025-06-12 19:02:27 +0800

    重新整理cloud与mmylxl的pom结构

---
+++ cloud/payment8001/src/main/java/org/jalontsui/cloud/Main8001.java
@@ -0,0 +1,13 @@
+package org.jalontsui.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("org.jalontsui.cloud.mapper")
+public class Main8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8001.class, args);
+    }
+}
---
+++ cloud/payment8001/src/main/java/org/jalontsui/cloud/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package org.jalontsui.cloud.mapper;
+
+import org.jalontsui.cloud.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
